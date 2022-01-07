ffd74f4ec66c842e9128b4c1c8468ed34bc9d08f
robiulhasanrohan <robiul@blogdesire.com>
2022-01-07 20:57:58 +0600

    Updates

---
+++ precache-manifest.16d42de17267b61f5a6161ec8c7859a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "371445b32bf4b8a033d4",
+    "url": "/tesla-clone/static/css/main.e58f99b8.chunk.css"
+  },
+  {
+    "revision": "371445b32bf4b8a033d4",
+    "url": "/tesla-clone/static/js/main.371445b3.chunk.js"
+  },
+  {
+    "revision": "239220a5aa7e83fadab4",
+    "url": "/tesla-clone/static/js/1.239220a5.chunk.js"
+  },
+  {
+    "revision": "148239be0cad213eacd5",
+    "url": "/tesla-clone/static/js/runtime~main.148239be.js"
+  },
+  {
+    "revision": "632842592110b4b02ce140450fa79db3",
+    "url": "/tesla-clone/index.html"
+  }
+];
